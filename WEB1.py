6cd0582f5569fa2f036e94b5b5b4b4111bb311e4
Dharshini-457 <dharshinir619@gmail.com>
2025-02-21 18:26:20 +0530

    commit2

---
+++ WEB1.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('web.html') # Make sure index.html is in the 'templates' folder
+
+@app.route('/about')
+def about():
+    return render_template('about.html') # Another page, for example
+
+@app.route('/info')
+def contact():
+    return render_template('info.html') # Contact page, another HTML template
+
+if __name__ == '__main__':
+   app.run(debug=True)
